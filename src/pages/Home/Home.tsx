e4ecfe8fed051e8700eafb5771594526e81ba671
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-01-17 18:16:41 -0800

    created basic folder & file structure

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react';
+import styles from "./Home.module.scss";
+
+interface Props {
+    
+}
+
+export default function Home(props: Props): ReactElement {
+    return (
+        <div className={styles.home}>
+            
+        </div>
+    )
+}
