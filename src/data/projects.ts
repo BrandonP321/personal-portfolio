96d5feb121cc797e5aebd1d0cb063abe767a9135
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-02-26 18:28:36 -0800

    Added projects section to home page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,3 +1,53 @@
+import seasonLostImg from "../media/img/projects/season_lost_project.jpg";
+import witchQueenImg from "../media/img/projects/wq_project.jpg";
+import annivImg from "../media/img/projects/anniv_project.jpg";
+import freeToPlayImg from "../media/img/projects/free_to_play_project.jpg";
+import seasonSplicerImg from "../media/img/projects/s14_project.jpg";
+
 export const projects = [
-    
+    {
+        name: "Destiny 2: Free To Play",
+        descLong: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        descShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        siteUrl: "https://bungie.net/7/en/Destiny/FreeToPlay",
+        image: freeToPlayImg,
+        repoUrl: "",
+        eyebrow: "React, TypeScript, SCSS"
+    },
+    {
+        name: "Destiny 2: Season of the Splicer",
+        descLong: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        descShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        siteUrl: "https://bungie.net/SeasonOfTheSplicer",
+        image: seasonSplicerImg,
+        repoUrl: "",
+        eyebrow: "React, TypeScript, SCSS"
+    },
+    {
+        name: "Destiny 2: Season of the Lost",
+        descLong: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        descShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        siteUrl: "https://bungie.net/SeasonOfTheLost",
+        image: seasonLostImg,
+        repoUrl: "",
+        eyebrow: "React, TypeScript, SCSS"
+    },
+    {
+        name: "Destiny 2: The Witch Queen",
+        descLong: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        descShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        siteUrl: "https://bungie.net/WitchQueen",
+        image: witchQueenImg,
+        repoUrl: "",
+        eyebrow: "React, TypeScript, SCSS"
+    },
+    // {
+    //     name: "Destiny 2: 30th Anniversary",
+    //     descLong: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    //     descShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    //     siteUrl: "https://bungie.net/Anniversary",
+    //     image: annivImg,
+    //     repoUrl: "",
+    //     eyebrow: "React, TypeScript, SCSS"
+    // },
 ]
---
+++ src/pages/Home/components/Projects/Projects.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { faArrowRight } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import OutlinedSectionHeading from '../../../../components/OutlinedSectionHeading/OutlinedSectionHeading';
+import { projects } from '../../../../data/projects';
+import styles from "./Projects.module.scss";
+
+type Props = {};
+
+export default function Projects(props: Props) {
+  return (
+      <div className={styles.projects}>
+        <div className={styles.sectionContent}>
+          <OutlinedSectionHeading
+            heading={"PROJECTS"}
+            className={styles.sectionHeading}
+          />
+
+          {projects.map((p, i) => {
+            const isReverseFlex = i % 2 !== 0;
+
+            return (
+              <div 
+                key={i}
+                className={classNames(styles.projectBlock, {[styles.reverse]: isReverseFlex})}
+                
+              >
+                <div className={styles.textWrapper}>
+                  <p className={styles.eyebrow}>{p.eyebrow}</p>
+                  <h3 className={styles.title}>{p.name}</h3>
+                  <div className={styles.btnsWrapper}>
+                    <a 
+                      href={p.siteUrl} 
+                      target={"_blank"} 
+                      rel={"noreferrer"}
+                    >View This Site <FontAwesomeIcon icon={faArrowRight}/></a>
+                    {p.repoUrl &&
+                      <a href={p.repoUrl} target={"_blank"} rel={"noreferrer"}>GitHub Repo</a>
+                    }
+                  </div>
+                </div>
+                <a 
+                  className={styles.imgWrapper}
+                  href={p.siteUrl}
+                  target={"_blank"}
+                  rel={"noreferrer"}
+                >
+                  <div className={styles.tiltedBg}/>
+                  <div className={styles.innerImgWrapper}>
+                    <img className={styles.projectImg} src={p.image} alt={""}/>
+                  </div>
+                </a>
+              </div>
+            )
+          })}
+
+        </div>
+      </div>
+  );
+}
