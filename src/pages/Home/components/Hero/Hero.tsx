f407737a42f4aa7b7c830eb1327b06a4750c0a7a
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-02-26 18:28:47 -0800

    Added hero section to home page

---
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from "./Hero.module.scss";
+import heroLoc from "../../../../data/localization/Home.json"
+import Button from '../../../../components/Button/Button';
+import symbolBg from "../../../../media/img/hero/code-symbol.jpg";
+
+type Props = {};
+
+export default function Hero(props: Props) {
+    const loc = heroLoc.hero
+
+    return (
+        <div className={styles.hero}>
+            <div className={styles.contentWrapper} style={{ backgroundImage: `url(${symbolBg})` }}>
+                {/* <div className={styles.heroBg} /> */}
+                <div className={styles.textWrapper}>
+                    <h1 dangerouslySetInnerHTML={{ __html: loc.title }} />
+                    <p className={styles.blurb}>{loc.blurb}</p>
+                </div>
+                <div className={styles.btnGroup}>
+                    <Button animate>{loc.myWorkBtnTitle}</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
