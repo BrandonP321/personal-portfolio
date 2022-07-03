0562dea7978c6f961a1adfa5d2517879dce9b590
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-07-03 13:47:17 -0700

    Project block should hide 'site url' button if no link exists

--- src/pages/Home/components/Projects/Projects.tsx
+++ src/pages/Home/components/Projects/Projects.tsx
@@ -10,52 +10,53 @@ type Props = {};
 
 export default function Projects(props: Props) {
   return (
-      <div className={styles.projects}>
-        <div className={styles.sectionContent}>
-          <OutlinedSectionHeading
-            heading={"PROJECTS"}
-            className={styles.sectionHeading}
-          />
+    <div className={styles.projects}>
+      <div className={styles.sectionContent}>
+        <OutlinedSectionHeading
+          heading={"PROJECTS"}
+          className={styles.sectionHeading}
+        />
 
-          {projects.map((p, i) => {
-            const isReverseFlex = i % 2 !== 0;
+        {projects.map((p, i) => {
+          const isReverseFlex = i % 2 !== 0;
 
-            return (
-              <div 
-                key={i}
-                className={classNames(styles.projectBlock, {[styles.reverse]: isReverseFlex})}
-                
-              >
-                <div className={styles.textWrapper}>
-                  <p className={styles.eyebrow}>{p.eyebrow}</p>
-                  <h3 className={styles.title}>{p.name}</h3>
-                  <div className={styles.btnsWrapper}>
-                    <a 
-                      href={p.siteUrl} 
-                      target={"_blank"} 
+          return (
+            <div
+              key={i}
+              className={classNames(styles.projectBlock, { [styles.reverse]: isReverseFlex })}
+            >
+              <div className={styles.textWrapper}>
+                <p className={styles.eyebrow}>{p.eyebrow}</p>
+                <h3 className={styles.title}>{p.name}</h3>
+                <div className={styles.btnsWrapper}>
+                  {p.siteUrl &&
+                    <a
+                      href={p.siteUrl}
+                      target={"_blank"}
                       rel={"noreferrer"}
-                    >View This Site <FontAwesomeIcon icon={faArrowRight}/></a>
-                    {p.repoUrl &&
-                      <a href={p.repoUrl} target={"_blank"} rel={"noreferrer"}>GitHub Repo</a>
-                    }
-                  </div>
+                    >View This Site <FontAwesomeIcon icon={faArrowRight} /></a>
+                  }
+                  {p.repoUrl &&
+                    <a href={p.repoUrl} target={"_blank"} rel={"noreferrer"}>GitHub Repo</a>
+                  }
                 </div>
-                <a 
-                  className={styles.imgWrapper}
-                  href={p.siteUrl}
-                  target={"_blank"}
-                  rel={"noreferrer"}
-                >
-                  <div className={styles.tiltedBg}/>
-                  <div className={styles.innerImgWrapper}>
-                    <img className={styles.projectImg} src={p.image} alt={""}/>
-                  </div>
-                </a>
               </div>
-            )
-          })}
+              <a
+                className={styles.imgWrapper}
+                href={p.siteUrl}
+                target={"_blank"}
+                rel={"noreferrer"}
+              >
+                <div className={styles.tiltedBg} />
+                <div className={styles.innerImgWrapper}>
+                  <img className={styles.projectImg} src={p.image} alt={""} />
+                </div>
+              </a>
+            </div>
+          )
+        })}
 
-        </div>
       </div>
+    </div>
   );
 }
