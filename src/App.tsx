5c36c449c5a87ae32cc6570b9a16613c47bdc526
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-07-09 02:06:10 -0700

    Added 404 redirect handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "destyle.css";
 import './App.scss';
 import Home from './pages/Home/Home';
@@ -10,6 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route path={"/"} element={<Home/>}/>
+          {/* 404 Redirects back to home page */}
+          <Route path={"*"} element={<Navigate replace to={"/"}/>}/>
         </Routes>
       </Router>
     </div>
